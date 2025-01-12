6672e6524e034b46510d8a571b4ee2d6f7531b23
J Wu <wujo635@gmail.com>
2025-01-12 18:38:46 -0500

    standup for localhost 8080

---
+++ src/main/java/org/wujo/rankEverything/HelloController.java
@@ -0,0 +1,12 @@
+package org.wujo.rankEverything;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings";
+    }
+}
