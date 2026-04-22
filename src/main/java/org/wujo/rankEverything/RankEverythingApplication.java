ad54a7f641ee0ca29cecabfc36e92933c0f2ac82
J Wu <wujo635@gmail.com>
2026-04-22 02:09:30 -0400

    remove command line runner

--- src/main/java/org/wujo/rankEverything/RankEverythingApplication.java
+++ src/main/java/org/wujo/rankEverything/RankEverythingApplication.java
@@ -1,12 +1,7 @@
 package org.wujo.rankEverything;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class RankEverythingApplication {
@@ -15,17 +10,4 @@ public class RankEverythingApplication {
 		SpringApplication.run(RankEverythingApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
 }
